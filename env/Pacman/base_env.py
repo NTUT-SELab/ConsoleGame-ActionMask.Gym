3d7fdd1326e4653deba61590ee00878a62e4bccd
sean2525 <madness48596@gmail.com>
2020-04-05 22:45:35 +0800

    Add ghost_agent game

--- env/Pacman/base_env.py
+++ env/Pacman/base_env.py
@@ -1,5 +1,8 @@
 import gym
 import numpy as np
+from .map import Map
+from .game import GhostRules, GameState
+from .ghost_agent import DirectionalGhost
 
 
 class BaseEnv(gym.Env):
@@ -7,6 +10,74 @@ class BaseEnv(gym.Env):
     A pacman environment.
 
     : param map_name: (str) 要運行的地圖和相關資訊
+    : param end_step: (int) 每個回合，最多可運行的步數
     """
-    def __init__(self, map_name='default_map'):
+    def __init__(self, map_name='default_map', end_step=10000):
+        self.map = Map(map_name)
+        self.end_step = end_step
+        self.state = GameState(self.map)
+        self.ghostAgents = [
+            DirectionalGhost(i)
+            for i in range(1, len(self.state.getNumAgents()))
+        ]
+        self.action_space = gym.spaces.Discrete(4)
+        self.obs_shape = (self.map.shape[0], self.map.shape[1], 1)
+        self.observation_space = gym.spaces.Box(low=0,
+                                                high=5,
+                                                shape=self.obs_shape,
+                                                dtype=np.float16)
+
+    def reset(self):
+        """
+        Reset environment.
+        """
+        self.state_cache = self.state.deepCopy()
+        self.current_step = 0
+        return Map.toObservation(self.map_cache, self.obs_shape)
+
+    def step(self, action):
+        """
+        Tell the environment which action to do.
+
+        : param action: (int) 要執行的動作
+        """
+        direction = Actions.getActionWithIndex(action)
+        reward = self.get_reward(direction)
+        done = self.is_done()
+
+        obs = self.state_cache.toObservation(self.obs_shape)
+        self.current_step += 1
+
+        return obs, reward, done, {}
+
+    def render(self, delay_time=1):
+        """
+        Print environment.
+
+        : param delay_time: (float) 每次打印要延遲的時間
+        """
         pass
+
+    def get_reward(self, action):
+        """
+        Give rewards based on actions state.
+
+        """
+        self.state_cache.scoreChange = 0
+
+        for ghost in self.ghostAgents:
+            GhostRules.applyAction(ghost.getAction(self.state_cache))
+            GhostRules.decrementTimer(
+                self.state_cache.getGhostState(ghost.index))
+
+        self.state_cache = self.state_cache.generateSuccessor(0, action)
+        return self.state_cache.scoreChange
+
+    def is_done(self):
+        """
+        Check if this round is over.
+
+        : param target_obj: (MapEnum) 老鼠前方ㄧ格的物件
+        """
+        return self.current_step >= self.end_step or self.state_cache.isWin(
+        ) or self.state_cache.isLose()
--- env/Pacman/map.py
+++ env/Pacman/map.py
@@ -1,6 +1,57 @@
 import os
 import numpy as np
-from env.Pacman.map_define import MapEnum
+from env.Pacman.map_define import MapEnum, MapObsEnum
+
+
+class Grid:
+    def __init__(self, width, height, initialValue=False):
+        if initialValue not in [False, True]:
+            raise Exception('Grids can only contain booleans')
+        self.width = width
+        self.height = height
+        self.data = np.array([[initialValue for y in range(height)]
+                              for x in range(width)])
+
+    def __getitem__(self, i):
+        return self.data[i]
+
+    def __setitem__(self, key, item):
+        self.data[key] = item
+
+    def __str__(self):
+        out = [[str(self.data[x][y])[0] for x in range(self.width)]
+               for y in range(self.height)]
+        out.reverse()
+        return '\n'.join([''.join(x) for x in out])
+
+    def __eq__(self, other):
+        if other == None:
+            return False
+        return self.data == other.data
+
+    def copy(self):
+        g = Grid(self.width, self.height)
+        g.data = np.array([x[:] for x in self.data])
+        return g
+
+    def deepCopy(self):
+        return self.copy()
+
+    def shallowCopy(self):
+        g = Grid(self.width, self.height)
+        g.data = self.data
+        return g
+
+    def count(self, item=True):
+        return self.data.sum()
+
+    def asList(self, key=True):
+        list = []
+        for x in range(self.width):
+            for y in range(self.height):
+                if self[x][y] == key:
+                    list.append((x, y))
+        return list
 
 
 class Map:
@@ -14,7 +65,6 @@ class Map:
          G - Ghost
          P - Pacman
     : param map_name: (str) 地圖檔案名稱
-    : param ghost_num: (int) 鬼的數量
     """
     def __init__(self, map_name):
         self.get_map(map_name)
@@ -27,9 +77,74 @@ class Map:
         with open(map_file, 'r') as f:
             map_buffer = np.asarray(f.read().splitlines(), dtype='c')
             map_buffer = map_buffer.astype(str)
-        self._map = self.check_map(map_buffer)
-        self.width = len(self.map[0])
-        self.height = len(self.map)
+        self.data = self.check_map(map_buffer)
+        self.shape = self.data.shape
+        self.map_name = map_name
+        self.width = len(self.data[0])
+        self.height = len(self.data)
+        self.agentPositions = []
+        self.capsules = []
+        self.numGhosts = 0
+        self.food = Grid(self.width, self.height)
+        self.walls = Grid(self.width, self.height)
+        self.processLayoutText(self.data)
+
+    @staticmethod
+    def toObservation(map_data, shape):
+        """
+        Convert map data to neural network input formate.
+
+        : param map_data:   (list)      整張地圖的集合
+        : param shape:      (obs_shape) 神經網路輸入的形狀
+        """
+        map_data = map_data.copy()
+        map_data[map_data == MapEnum.road.value] = MapObsEnum.road.value
+        map_data[map_data == MapEnum.ghost.value] = MapObsEnum.ghost.value
+        map_data[map_data == MapEnum.wall.value] = MapObsEnum.wall.value
+        map_data[map_data == MapEnum.pacman.value] = MapObsEnum.pacman.value
+        map_data[map_data ==
+                 MapEnum.capsules.value] = MapObsEnum.capsules.value
+        map_data[map_data == MapEnum.food.value] = MapObsEnum.food.value
+
+        return np.reshape(map_data.astype(np.float16), shape)
+
+    def processLayoutText(self, layoutText):
+        """
+        Coordinates are flipped from the input format to the (x,y) convention here
+        The shape of the maze.  Each character
+        represents a different type of object.
+         % - Wall
+         . - Food
+         o - Capsule
+         G - Ghost
+         P - Pacman
+        Other characters are ignored.
+        """
+        maxY = self.height - 1
+        for y in range(self.height):
+            for x in range(self.width):
+                layoutChar = layoutText[maxY - y][x]
+                self.processLayoutChar(x, y, layoutChar)
+        self.agentPositions.sort()
+        self.agentPositions = [(i == 0, pos) for i, pos in self.agentPositions]
+
+    def processLayoutChar(self, x, y, layoutChar):
+        if layoutChar == '%':
+            self.walls[x][y] = True
+        elif layoutChar == '.':
+            self.food[x][y] = True
+        elif layoutChar == 'o':
+            self.capsules.append((x, y))
+        elif layoutChar == 'P':
+            self.agentPositions.append((0, (x, y)))
+        elif layoutChar in ['G']:
+            self.agentPositions.append((1, (x, y)))
+            self.numGhosts += 1
+
+    def __str__(self):
+        out = [[str(self.data[y][x])[0] for x in range(self.width)]
+               for y in range(self.height)]
+        return '\n'.join([''.join(x) for x in out])
 
     def check_map(self, map_data):
         """
@@ -61,8 +176,14 @@ class Map:
 
         return map_data
 
+    def isWall(self, pos):
+        x, col = pos
+        return self.walls[x][col]
+
     def reset(self):
         """
         Reset map.
         """
-        self.map_cache = np.copy(self._map)
+        map_cache = np.copy(self.data)
+        self.get_map(self.map_name)
+        return map_cache
