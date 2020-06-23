9218fc866a99618727f7f71056e584b29b61280a
Mino <madness48596@gmail.com>
2020-06-23 15:24:38 +0800

    Remove nearestPoint

--- tests/Bomberman/test_utils.py
+++ tests/Bomberman/test_utils.py
@@ -1,11 +1,6 @@
 from env.Bomberman import utils
 
 
-def test_nearestPoint():
-    assert (1, 1) == utils.nearestPoint((1, 1))
-    assert (2, 2) == utils.nearestPoint((1.5, 1.5))
-
-
 def test_manhattanDistance():
     assert utils.manhattanDistance((-1, 1), (0, 0)) == 2
     assert utils.manhattanDistance((2, -1), (0, -3)) == 4
